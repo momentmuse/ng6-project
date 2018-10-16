5a83bb25ef6a34d360adfd2ebfeaa3f7a25ec0a3
Amy Kirasack <amykirasack@gmail.com>
2018-10-16 23:47:37 +0200

    Set up routing and rendering to page for users, details, and posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,12 @@ import { PostsComponent } from './posts/posts.component';
 import { UsersComponent } from './users/users.component';
 import { DetailsComponent } from './details/details.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    PostsComponent,
-    UsersComponent,
-    DetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SidebarComponent, PostsComponent, UsersComponent, DetailsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
